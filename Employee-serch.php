7135d9b536464ab1cd6e3ed2878c5f55083e35cb
j-morgan6 <103053298+j-morgan6@users.noreply.github.com>
2023-08-02 14:14:54 -0400

    Update Employee-serch.php

--- Employee-serch.php
+++ Employee-serch.php
@@ -5,6 +5,15 @@ table, th, td {
 }
 </style>
 <body>
+<h1 class="my-5">Hi, Welcome to the University Bank System.</h1>
+    <p><nav class="nav justify-content-center">
+    <a href="home.php">Home</a>
+    <a href="Customer-serch.php">Customer-serch</a>
+    <a href="Bank-serch.php">Bank-serch</a>
+    <a href="Accounts-serch.php">Accounts-serch</a>
+    <a href="Loan-serch.php">Loan-serch</a>
+    <a href="Employee-serch.php">Employee-serch</a>
+</nav>
 <p><h2>University Bank Search:</h2></p>
 <form action="Employee-serch.php" method=get>
 	Enter Employee_ID: <input type=text size=20 name="Employee_ID">
