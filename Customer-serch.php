dd232645f09e75648b3c3ae81974f5718361c015
j-morgan6 <103053298+j-morgan6@users.noreply.github.com>
2023-08-02 15:46:55 -0400

    Update Customer-serch.php

--- Customer-serch.php
+++ Customer-serch.php
@@ -5,6 +5,15 @@ table, th, td {
 }
 </style>
 <body>
+<h1 class="my-5">Hi, Welcome to the University Bank System.</h1>
+   <p><nav class="nav justify-content-center">
+   <a href="home.php">Home</a>
+   <a href="Customer-serch.php">Customer-serch</a>
+   <a href="Bank-serch.php">Bank-serch</a>
+   <a href="Accounts-serch.php">Accounts-serch</a>
+   <a href="Loan-serch.php">Loan-serch</a>
+   <a href="Employee-serch.php">Employee-serch</a>
+</nav>
 <p><h2>University Bank Search:</h2>
 <h3><a href="Delete-customer.php">Delete-customer</a></h3></p>
 <form action="Customer-serch.php" method=get>
