e238c3b2fb2f5d19d07f6b30d79f168fd323d934
RonganBai <140551985+RonganBai@users.noreply.github.com>
2023-08-02 14:05:10 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>UBS home page</title>
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+		table, th, td { border: 1px solid black; }
+		.center {  margin-left: auto;  margin-right: auto;}
+    </style>
+</head>
+<body>
+<h1 class="my-5">Hi, Welcome to the University Bank System.</h1>
+    <p><nav class="nav justify-content-center">
+    <a href="home.php">Home</a>
+    <a href="Customer-serch.php">Customer-serch</a>
+    <a href="Bank-serch.php">Bank-serch</a>
+    <a href="Accounts-serch.php">Accounts-serch</a>
+    <a href="Loan-serch.php">Loan-serch</a>
+    <a href="Employee-serch.php">Employee-serch</a>
+</nav>
+</body>
+</html>
